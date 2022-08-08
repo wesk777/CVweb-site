0e22691ef1d89131ef0a98181189d44849820366
sergeev <sergeevim24@gmail.com>
2022-08-08 21:55:49 +0300

    add docker

--- main.py
+++ main.py
@@ -39,8 +39,5 @@ def certificates():
     return render_template("certificates.html")
 
 
-
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
